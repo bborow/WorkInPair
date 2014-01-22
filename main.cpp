74fefa3a4e06549c66afe7563de82e96da524b48
Bartosz Borowiecki <bartosz.borowiecki@gmail.com>
2014-01-22 12:13:59 +0100

    Modified generator main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "utils.h"
+#include "generator.h"
 
 int main()
-{
+{	int *tab = generate();
+	int size = sizeof(tab);
+	printall(tab, size);
+	std::cout << std::endl;
+	std::cout << "Min value: " << min(tab, size);
+	std::cout << std::endl;
 	return 0;
 }
