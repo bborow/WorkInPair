92f749ba88e6779338e605c08fc248d48cd568f3
damianeq89 <damianeq89@gmail.com>
2014-01-22 12:00:55 +0100

    added min to utils

--- utils.h
+++ utils.h
@@ -7,3 +7,14 @@ void printall(int *tab, int size)
 		std::cout << tab[i] << ' ';
 	}
 }
+
+int min(int *tab, int size)
+{
+	int min = tab[0];
+	for(int i = 1; i < size; i++)
+	{
+		if(tab[i] < min)
+			min = tab[i];
+	}
+	return min;
+}
